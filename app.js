70bf34a30fb0f2eba632a9a2f58dc6e6f9bf5e57
Keishon Smith <keishon104@gmail.com>
2019-04-19 17:39:54 -0400

    Working Server and Connected Database

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
-var firebase = require("firebase-admin");
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const firebase = require("firebase-admin");
+
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+app.listen(port, function(){
+  console.log('Welcome to my Database.');
+})
+
+//sets view engine
+app.use(express.static(__dirname + '/client'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 var serviceAccount = require("./serviceAccountKey.json");
 
+// Initalizes the database.
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://test-23cfc.firebaseio.com"
+  databaseURL: "https://daroushproject.firebaseio.com"
 });
-
 var db = firebase.database();
 var ref = db.ref("restricted_access/secret_document");
-ref.once("value", function(snapshot) {
-  console.log(snapshot.val());
+
+
+app.get('/', (req, res) => {
+
+  console.log("it Works");
+
+  res.send({ hello: 'world' });
 });
 
-var usersRef = ref.child("users");
-usersRef.set({
-  alanisawesome: {
-    date_of_birth: "June 23, 1912",
-    full_name: "Alan Turing"
-  },
-  gracehop: {
-    date_of_birth: "December 9, 1906",
-    full_name: "Grace Hopper"
-  }
+app.post('/accounts', (req, res) => {
+  ref.once("value", function(snapshot) {
+    console.log(snapshot.val());
+  });
+
+  var usersRef = ref.child("users");
+  usersRef.set({
+    customer_1: {
+      username: "testof122",
+      password: "password122"
+    },
+    customer_2: {
+      username: "testof2",
+      password: "password2"
+    }
+  });  
 });
